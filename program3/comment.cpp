dae48f12955d08b1427ee8b73bbef5dfd39f325c
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 10:06:15 +0200

    types of comment

---
+++ program3/comment.cpp
@@ -0,0 +1,16 @@
+// A simple program to show the use of comment
+#include <iostream>
+using namespace std;
+/*
+This is 
+a 
+multiple line
+comment
+*/
+int main()
+{
+    // printing my name
+    cout << "Anisul Islam" << endl;
+    // printing my phone
+    cout << "01710444700" << endl;
+}
