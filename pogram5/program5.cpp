76a4d8c4af35aecff913183ee776045fc3d76bbf
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 15:56:44 +0200

    string variable

---
+++ pogram5/program5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    char name[13] = "Anisul Islam";
+
+    cout << "My name is : " << name  <<endl ; 
+    getch();
+}
