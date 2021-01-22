9fb526a3e1a4b998cbedbc56194101ce100ed480
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 22:33:30 +0200

    Unary operator

---
+++ program12/program12.cpp
@@ -0,0 +1,17 @@
+// Unary Operator -> + (unary plus), - (unary minus), ++ , -- 
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() 
+{
+  
+    int x = 5;
+    cout << -x << endl;
+    cout << x++ << endl;
+    cout << ++x << endl;
+    cout << x-- << endl;
+    cout << --x << endl;
+
+    getch();
+}
