642d89ca1c94c950bb7af896940eba34be8796f2
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 09:54:05 +0200

    escape sequence

---
+++ program2/program2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    cout << "Anisul Islam.\nM.Sc. in Software Development\n";
+
+
+    cout << "I am from Bangladesh. My Phone number is \t 01444" << endl;
+    cout << "I am currently living in Finaland";
+    getch();
+}
