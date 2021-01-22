84ceacf092fdd286095b6e3031073527a2abb063
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 16:23:45 +0200

    Arithmetic operator

--- program7/program7.cpp
+++ program7/program7.cpp
@@ -12,20 +12,20 @@ int main()
     cin >> num2;
 
     result = num1 + num2;
-    cout << "sum is : " << result;
+    cout << "sum is : " << result << endl;
 
     result = num1 - num2;
-    cout << "sub is : " << result;
+    cout << "sub is : " << result << endl;
 
     result = num1 * num2;
-    cout << "Mul is : " << result;
+    cout << "Mul is : " << result << endl;
 
     result = num1 / num2;
-    cout << "div is : " << result;
+    cout << "div is : " << result <<endl;
 
 
     result = num1 % num2;
-    cout << "Remainder is : " << result;
+    cout << "Remainder is : " << result <<endl;
 
 
     getch();
