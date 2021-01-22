083beffc4e2ea9174b148b9e23a0135753ec3fc2
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 10:13:08 +0200

    keyword, variable, data-type

---
+++ program4/program4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int id, age;
+    float cgpa = 3.92;
+
+    id = 1302020017;
+    age = 31;
+
+    cout << "Student Id : " << id << endl;
+    cout << "Student age: " << age << endl;
+    cout << "Student cgpa: " << cgpa << endl;
+
+    getch();
+
+}
