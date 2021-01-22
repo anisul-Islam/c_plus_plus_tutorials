085678c45886ef3ea39fecbefbeeffc4dacd4fe7
Anisul Islam <anisul.islam@tuni.fi>
2021-01-22 22:27:14 +0200

    Assignment operator

---
+++ program11/program11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() 
+{
+    int x = 3;
+    int y = 2;
+
+    x += y;
+    cout << x << endl;
+
+    x -= y;
+    cout << x << endl;
+
+    x *= y;
+    cout << x << endl;
+
+    x /= y;
+    cout << x << endl;
+
+    x %= y;
+    cout << x << endl;
+
+    getch();
+}
