0b5850dd4c6f64a61c551c5e44ed03223312c227
Anisul Islam <anisul.islam@tuni.fi>
2021-01-23 19:59:21 +0200

    do while loop

---
+++ program25/program25.cpp
@@ -0,0 +1,20 @@
+
+// do while loop
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    int x=1;
+
+  
+    do {
+        cout << x << endl; 
+        x++;
+    }  while(x<=10);
+
+    getch();
+}
