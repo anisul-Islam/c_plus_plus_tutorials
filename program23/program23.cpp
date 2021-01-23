a1a2a05635b911ea3425d09cc44bb57bc21ec76a
Anisul Islam <anisul.islam@tuni.fi>
2021-01-23 19:56:43 +0200

    for loop

---
+++ program23/program23.cpp
@@ -0,0 +1,19 @@
+
+//for loop
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+
+    int x;
+
+    for(x=1; x<=10; x++)
+    {
+        cout << x << endl; 
+    }
+
+    getch();
+}
