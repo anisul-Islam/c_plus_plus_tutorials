e3be027a3f95fc5c84b3b3a6f0d5c2e651ebef71
Anisul Islam <anisul.islam@tuni.fi>
2021-01-23 20:26:51 +0200

    Introduction to Array

---
+++ program30/program30.cpp
@@ -0,0 +1,35 @@
+
+// pattern program
+/*
+n=3
+*
+**
+***
+*/
+
+#include<iostream>
+// Array basics
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    // declaring an array
+    int marks[5];
+    int x;
+
+    //initializing an array
+    marks[0] = 35;
+    marks[1] = 45;
+    marks[2] = 33;
+    marks[3] = 83;
+    marks[4] = 93;
+
+    // printing an array
+    for(x=0; x<5; x++)
+    {
+        cout << marks[x] << endl;
+    }
+
+    getch();
+}
