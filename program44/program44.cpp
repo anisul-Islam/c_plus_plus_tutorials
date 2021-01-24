038a381502edbe55549c90267d93f7bf5b069162
Anisul Islam <anisul.islam@tuni.fi>
2021-01-24 10:26:24 +0200

    adding parametrized function to class

---
+++ program44/program44.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Student 
+{
+
+    public:
+        //variables
+        int id;
+        double gpa;
+        //functions
+        void display()
+        {
+            cout << id << "  " << gpa << endl;
+        }
+
+        // default constructor
+        Student()
+        {
+            cout << "This is a default constructor" << endl;
+        }
+
+        // parametrized constructor
+        Student(int x, double y)
+        {
+           id = x;
+           gpa = y;
+        }
+
+};
+
+int main()
+{
+
+    Student();
+    
+    cout << "ID" << "  " << "GPA" << endl;
+    cout << "-----------"<< endl;
+
+    Student s1(101,3.92);
+    s1.display();
+
+    Student s2(102,3.44);
+    s2.display();
+
+    getch();
+}
